da9ed7229a7e93c35291db1fab766d01305a43d2
skdariyasafru <sk.dariyasafru@gmail.com>
2026-09-17 18:25:38 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Project Started Successfully 🚀"
+
+
+@app.route("/health")
+def health():
+    return {"status": "ok"}
+
+
+if __name__ == "__main__":
+    app.run()
