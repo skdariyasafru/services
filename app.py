df4e8a6e87aefa41c83eccd439b172d50121aa03
skdariyasafru <sk.dariyasafru@gmail.com>
2026-09-18 20:43:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
 from flask import Flask, render_template
+from config import Config
+from db import db
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+db.init_app(app)
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/ping")
+def ping():
+    return "Service Project is running!"
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000, debug=True)
+    app.run(host="0.0.0.0", port=10000)
